5d31308c1f86bfea7c98e692f6f005014af6713e
nijamkhansherani <nijamkhansherani@gmail.com>
2023-03-13 10:18:22 +0530

    first commit

--- backend/Model/wishlishModel.js
+++ backend/Model/wishlishModel.js
@@ -3,11 +3,8 @@ const wishlish = new mongoose.Schema({
     title: {  type: String },
     dis: { type: String },
     price: {type: Number},
-    reting: {type: String},
-    color: { type: String},
     img: {type: String},
-    size:{ type:String },
-    liteimage:{type:String},
+    
 }, 
 {
     timestamps: true
--- backend/controllers/wishlishcontrollers.js
+++ backend/controllers/wishlishcontrollers.js
@@ -1,3 +1,4 @@
+const { route } = require("../routers/wishlishrouters")
 
 const jwt = require("jsonwebtoken")
 const mongodb = require('mongodb');
@@ -7,17 +8,22 @@ const asyncHandler = require('express-async-handler');
 
 
 const getwishlish = async (req, res) => {
-    let data = await wishlish.findOne({});
-    console.log("======>", data);
-    res.status(200).json(data)
-}
-
+    let data = await wishlish.find({ user_id: req.user});
+    
+    if(!data.length){
+        res.status(200).json({ status: false, massage: "cart is empty"})
+    }
+    console.log("====>", data);
+    res.status(200).json({ status: true, massage: data})
+    
+    }
+    
 
 
 const postwishlish = async (req, res) => {
 
-    const { title, dis, price, reting,img,color,size, liteimage } = req.body
-    if (!title && !dis && !price && !reting && !img && !color && !size && !liteimage ) {
+    const { title, dis, price,img} = req.body
+    if (!title && !dis && !price && !img ) {
         res.status(400).json({ message: "Please add all Filed" })
     }
     let data = await wishlish.create({ 
@@ -25,17 +31,28 @@ const postwishlish = async (req, res) => {
         dis,
         img,
         price,
-        reting,
-        color,
-        size,
-        liteimage
+        user_id:req.user
+
     });
     console.log("====>", data);
     res.status(200).json(data)
 }
 
+const deletewishlish=  async(req,res)=>{
+   
+    let findid = await wishlish.findById(req.params._id);
+    if(!findid){
+        res.status(400);
+        res.send("user not found");
+    }
+        await findid.remove();
+       
+    res.status(200).json({message : `delete data ${req.params.id}`});
+ }
+
+
 
 
 module.exports = {
-    getwishlish, postwishlish,
+    getwishlish, postwishlish,deletewishlish
 }
